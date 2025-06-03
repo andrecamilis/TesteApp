f7b7f960629f4e0157d2245ea75455bc2e7cdd92
andre <andrecamilis@gmail.com>
2025-06-03 00:26:52 -0300

    First

---
+++ app/src/main/java/com/example/testeapp/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.testeapp
+
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import java.util.Random
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Solicitar permissão se necessário
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (checkSelfPermission(android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(arrayOf(android.Manifest.permission.POST_NOTIFICATIONS), 1001)
+            }
+        }
+
+
+
+    }
+
+    fun raffle(view: View) {
+        val textoResultado = findViewById<TextView>(R.id.text_resultado)
+        val numero = Random().nextInt(11)
+        textoResultado.text = "Number generated: $numero"
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/testeapp/Usuario.java
@@ -0,0 +1,8 @@
+package com.example.testeapp;
+
+public class Usuario {
+    public void logar(){
+        new Mensagem().enviar();
+
+    }
+}
